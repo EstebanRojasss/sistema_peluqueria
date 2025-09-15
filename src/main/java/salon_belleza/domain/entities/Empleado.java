2ceddbb54f96e8424f26059e257353f48c0754b2
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-15 12:33:10 -0300

    Agregar clase Empleado

---
+++ src/main/java/salon_belleza/domain/entities/Empleado.java
@@ -0,0 +1,38 @@
+package salon_belleza.domain.entities;
+
+import java.util.UUID;
+
+public class Empleado {
+
+    private final String id;
+    private String nombre;
+    private String telefono;
+
+
+    public Empleado(String nombre, String telefono){
+        UUID uuid = UUID.randomUUID();
+        this.id = uuid.toString();
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
