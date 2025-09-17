c98e107d3dd7af6fa8b4770ed918bf541aba16f3
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-17 19:38:39 -0300

    Agregar nuevo metodo para generalizar la tarea de ejecutar una persistencia en la base de datos

--- src/main/java/salon_belleza/infraestructure/daos/AbstractDao.java
+++ src/main/java/salon_belleza/infraestructure/daos/AbstractDao.java
@@ -1,4 +1,27 @@
 package salon_belleza.infraestructure.daos;
 
-public class AbstractDao {
+import salon_belleza.infraestructure.daos.jdbcCommonTemplates.ArgumentPreparedStatementSetter;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class AbstractDao {
+
+    protected ArgumentPreparedStatementSetter argumentSetter;
+    protected DataSource dataSource;
+
+    public AbstractDao(ArgumentPreparedStatementSetter argumentSetter, DataSource dataSource) {
+        this.argumentSetter = argumentSetter;
+        this.dataSource = dataSource;
+    }
+
+    protected void executeSave(String sql, Object... params) throws SQLException {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            argumentSetter.create(params).setValues(ps);
+        } catch (RuntimeException e) {
+            throw new SQLException("Ocurrio un error con la persistencia de los datos");
+        }
+    }
 }
