4de58a5b84339cdb0042c14970b4586a01c061e9
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-24 15:16:58 -0300

    Importacion de paquetes de clase Set

--- src/main/java/salon_belleza/infraestructure/daos/AbstractDao.java
+++ src/main/java/salon_belleza/infraestructure/daos/AbstractDao.java
@@ -7,8 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class AbstractDao<T> {
 
