f97b2152085f5068a246199c90795a8c651f60e2
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-25 17:04:55 -0300

    refactor: Cambios minimos e irrelevantes en el orden de los parametros en la clase Cliente

--- src/main/java/salon_belleza/domain/entities/Cliente.java
+++ src/main/java/salon_belleza/domain/entities/Cliente.java
@@ -8,17 +8,17 @@ public class Cliente {
     private final String id;
     private String nombre;
     private String telefono;
-    private String dni;
     private LocalDate fecha_regsitro;
+    private String dni;
 
 
 
-    public Cliente(String nombre, String telefono, String dni, LocalDate fecha_registro){
+    public Cliente(String nombre, String telefono,LocalDate fecha_registro,String dni){
         UUID uuid = UUID.randomUUID();
         this.id = uuid.toString();
         this.telefono = telefono;
-        this.dni = dni;
         this.fecha_regsitro = fecha_registro;
+        this.dni = dni;
     }
 
     public void setNombre(String nombre) {
