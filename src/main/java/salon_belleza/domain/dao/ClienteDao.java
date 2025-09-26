65ccbd7c507e753453799c34b465694d1aa9b42a
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-25 23:25:39 -0300

    Cambiar parametro de entrada de los metodos a -> «Cliente cliente»

--- src/main/java/salon_belleza/domain/dao/ClienteDao.java
+++ src/main/java/salon_belleza/domain/dao/ClienteDao.java
@@ -6,12 +6,13 @@ import java.util.Set;
 
 public interface ClienteDao {
     void saveClient(Cliente cliente);
-    Cliente findClientByName(String name);
+    Cliente findClientByName(String nombre);
     Cliente findClientByDni(String cliente);
     Cliente findClientById(String id);
-    void deleteClienteById(String id);
-    void deleteClienteByName(String name);
-    void deleteClienteByDni(String dni);
-    void updateClienteByName(String name);
+    void deleteClienteById(Cliente cliente);
+    void deleteClienteByName(Cliente cliente);
+    void deleteClienteByDni(Cliente cliente);
+    void updateClienteByName(Cliente cliente);
+    void updateClienteByID(Cliente cliente);
     Set<Cliente>findAllClients();
 }
