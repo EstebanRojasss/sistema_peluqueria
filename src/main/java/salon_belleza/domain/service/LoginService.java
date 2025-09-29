f7bcbda9ca8cc49e0fb2689c38a0e19daf00e9c1
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-29 18:36:58 -0300

    Cambiar el tipo de retorno a -> String

--- src/main/java/salon_belleza/domain/service/LoginService.java
+++ src/main/java/salon_belleza/domain/service/LoginService.java
@@ -2,7 +2,7 @@ package salon_belleza.domain.service;
 
 public interface LoginService {
 
-    void encryptPasswd(String passwd);
+    String encryptPasswd(String passwd);
 
     boolean checkPasswd(String textPlane, String passwd);
 
