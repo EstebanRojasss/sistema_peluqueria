0e0e07042082ea58c614b9e97a172691bdbc1ea2
ESTEBAN <estebanrojasch.2@gmail.com>
2025-09-30 17:40:15 -0300

    fix: Agregar parametro faltante a la consulta sql

--- src/main/java/salon_belleza/infraestructure/daos/UsuarioDaoImpl.java
+++ src/main/java/salon_belleza/infraestructure/daos/UsuarioDaoImpl.java
@@ -6,7 +6,7 @@ import salon_belleza.domain.entities.Usuario;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -29,63 +29,63 @@ public class UsuarioDaoImpl extends AbstractDao<Usuario> implements UsuarioDao {
 
     @Override
     public void save(Usuario usuario) {
-            String sql = "INSERT INTO user (id_user, name, password) VALUES(?, ?, ?)";
-            executeSave(sql, usuario.getId(), usuario.getNombre(), usuario.getContrasenha());
+        String sql = "INSERT INTO user (id_user, name, password) VALUES(?, ?, ?)";
+        executeSave(sql, usuario.getId(), usuario.getNombre(), usuario.getContrasenha());
     }
 
     @Override
     public Set<Usuario> findAllAdmins() {
-            String sql = """
-                    SELECT u.name, u.password FROM user u
-                    INNER JOIN rol_user ru ON u.id_user = ru.user_id\s
-                    INNER JOIN rol r ON r.id_rol = ru.rol_id
-                    WHERE r.role_name = ?
-                    """;
-            return executeFindAll(sql, "ADMIN");
+        String sql = """
+                SELECT u.id_user,u.name, u.password FROM user u
+                INNER JOIN rol_user ru ON u.id_user = ru.user_id\s
+                INNER JOIN rol r ON r.id_rol = ru.rol_id
+                WHERE r.role_name = ?
+                """;
+        return executeFindAll(sql, "ADMIN");
     }
 
     @Override
     public Set<Usuario> findAllEmployeers() {
-            String sql = """
-                    SELECT u.name, u,password FROM user u\s
-                    INNER JOIN rol_user ru ON u.id_user = ru.user_id\s
-                    INNER JOIN rol r ON r.id_rol = ru.rol_id
-                    WHERE r.role_name = ?
-                    """;
-            return executeFindAll(sql, "EMPLOYEE");
+        String sql = """
+                SELECT u.id_user,u.name, u,password FROM user u\s
+                INNER JOIN rol_user ru ON u.id_user = ru.user_id\s
+                INNER JOIN rol r ON r.id_rol = ru.rol_id
+                WHERE r.role_name = ?
+                """;
+        return executeFindAll(sql, "EMPLOYEE");
     }
 
     @Override
     public Usuario findById(String id) {
-            String sql = """
-                    SELECT u.name, u.password FROM user u WHERE u.id_user = ?
-                    """;
-            return executeFindByID(sql, id);
+        String sql = """
+                SELECT u.id_user,u.name, u.password FROM user u WHERE u.id_user = ?
+                """;
+        return executeFindByID(sql, id);
     }
 
     @Override
     public void update(Usuario usuario) {
-            String sql = """
-                    UPDATE user u\s
-                    SET name = ?
-                    WHERE id_user = ?
-                    """;
-            executeUpdate(sql, usuario.getId());
+        String sql = """
+                UPDATE user u\s
+                SET name = ?
+                WHERE id_user = ?
+                """;
+        executeUpdate(sql, usuario.getId());
     }
 
     @Override
     public void delete(Usuario usuario) {
-            String sql = """
-                    DELETE FROM user WHERE id_user = ?
-                    """;
-            exeuteDelete(sql, usuario.getId());
+        String sql = """
+                DELETE FROM user WHERE id_user = ?
+                """;
+        executeDelete(sql, usuario.getId());
 
     }
 
     @Override
-    public Usuario findByName(String name) {
-            String sql = "SELECT u.name FROM user u WHERE u.name = ?";
-            return executeFindByName(sql, name);
+    public List<Usuario> findByName(String name) {
+        String sql = "SELECT u.id_user,u.name, u.password FROM user u WHERE u.name = ?";
+        return executeFindByName(sql, name);
     }
 
     @Override
