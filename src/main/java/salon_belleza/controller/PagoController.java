f5683bb116f8ab37348a6a20b0befebefd8fb106
ESTEBAN <estebanrojasch.2@gmail.com>
2025-10-10 18:29:10 -0300

    Agregar controlador y vista para el modulo de Pagos

---
+++ src/main/java/salon_belleza/controller/PagoController.java
@@ -0,0 +1,19 @@
+package salon_belleza.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleGroup;
+
+public class PagoController {
+
+    public void abrirCaja(ActionEvent actionEvent) {
+    }
+
+    public void nuevoClienteRapido(ActionEvent actionEvent) {
+    }
+
+    public void nuevaVenta(ActionEvent actionEvent) {
+    }
+
+    public void procesarVenta(ActionEvent actionEvent) {
+    }
+}
